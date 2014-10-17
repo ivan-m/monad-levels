71cbed168339d8a833a9461fec653d530fdb0b1d
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2014-10-17 21:24:27 +1100

    Base functionality now typechecks
    Doesn't actually work though, as you can't pass around a value of
    kind ((* -> *) -> Constraint).

--- Control/Monad/Levels.hs
+++ Control/Monad/Levels.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE ConstraintKinds, DataKinds, FlexibleContexts, FlexibleInstances,
              KindSignatures, MultiParamTypeClasses, PolyKinds, RankNTypes,
              ScopedTypeVariables, TypeFamilies, UndecidableInstances #-}
@@ -42,18 +43,62 @@ lift m = wrap $ \ _unwrap addI -> addI m
 
 data Nat = Zero | Suc Nat
 
-class SatisfyConstraint (n :: Nat) (m :: * -> *) (c :: (* -> *) -> Constraint) where
+class SatisfyConstraint_ (n :: Nat) (c :: (* -> *) -> Constraint) (m :: * -> *) where
 
   _lower :: Proxy c -> Proxy n -> (forall m'. (c m') => m' a) -> m a
 
-instance (c m) => SatisfyConstraint Zero m c where
+instance (ConstraintSatisfied c m ~ True, c m) => SatisfyConstraint_ Zero c m where
 
   _lower _ _ m = m
 
-instance (MonadLevel m, SatisfyConstraint n (LowerMonad m) c)
-         => SatisfyConstraint (Suc n) m c where
+instance (MonadLevel m, SatisfyConstraint_ n c (LowerMonad m))
+         => SatisfyConstraint_ (Suc n) c m where
 
   _lower _ _ m = wrap (\ _unwrap addI -> addI (_lower (Proxy :: Proxy c) (Proxy :: Proxy n) m))
 
-data Proxy t = Proxy
+
+data Proxy (t :: k) = Proxy
              deriving (Show)
+
+-- data DictMonadConstraint ::
+
+type SatisfyConstraint c m = SatisfyConstraint_ (FindSatisfied c m) c m
+
+lower :: forall c m a. (SatisfyConstraint c m) =>
+         Proxy (c :: (* -> *) -> Constraint) -> (forall m'. (c m') => m' a) -> m a
+lower p m = _lower p (Proxy :: Proxy (FindSatisfied c m)) m
+
+type MonadConstraint c m = (Monad m, c m)
+
+-- -----------------------------------------------------------------------------
+
+type family FindLevel (c :: (* -> *) -> Constraint) (m :: (* -> *)) :: Nat
+
+-- type instance (c m) => FindLevel c m = Zero
+
+type family ConstraintSatisfied (c :: (* -> *) -> Constraint) (m :: * -> *) :: Bool
+
+type TrySatisfy (c :: (* -> *) -> Constraint) (m :: (* -> *)) = TrySatisfy' c (BaseMonad m) m
+
+type family TrySatisfy' (c :: (* -> *) -> Constraint) (b :: (* -> *)) (m :: (* -> *)) :: [Bool] where
+  TrySatisfy' c b b = '[ ConstraintSatisfied c b ]
+  -- Need this first in case of transformers that are not instance of MonadLevel
+  TrySatisfy' c b (t m) = (ConstraintSatisfied c (t m)) ': TrySatisfy' c b m
+  -- Still need this for newtype wrappers, etc.
+  TrySatisfy' c b m = (ConstraintSatisfied c m) ': TrySatisfy' c b (LowerMonad m)
+
+type family FindTrue (bs :: [Bool]) :: Nat where
+  FindTrue (True  ': t) = Zero
+  FindTrue (False ': t) = Suc (FindTrue t)
+
+type FindSatisfied (c :: (* -> *) -> Constraint) (m :: * -> *) = FindTrue (TrySatisfy c m)
+
+-- -----------------------------------------------------------------------------
+
+type IsBaseMonad m = (MonadTower m, m ~ BaseMonad m)
+
+type instance ConstraintSatisfied IsBaseMonad m = SameMonad (BaseMonad m) m
+
+type family SameMonad m n where
+  SameMonad m m = True
+  SameMonad m n = False
