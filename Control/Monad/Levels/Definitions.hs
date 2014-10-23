0ac192ee37f8cf19324e2546757822aeb563d597
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2014-10-23 18:39:29 +1100

    Add Monad{Tower,Level} instances for transformers
    All monad transformers from the transformers library now have instances
    for use with MonadTower and MonadLevel with the exception of ErrorT in
    favour of the new ExceptT transformer instead (hence the requirement for
    transformers >= 0.4).

--- Control/Monad/Levels/Definitions.hs
+++ Control/Monad/Levels/Definitions.hs
@@ -15,9 +15,21 @@ module Control.Monad.Levels.Definitions where
 
 import Control.Applicative
 
-import           Control.Monad.Trans.Cont       (ContT (..))
-import qualified Control.Monad.Trans.State.Lazy as LSt
+import           Control.Monad.Trans.Cont          (ContT (..))
+import           Control.Monad.Trans.Except        (ExceptT (..), runExceptT)
+import           Control.Monad.Trans.Identity      (IdentityT (..))
+import           Control.Monad.Trans.List          (ListT (..))
+import           Control.Monad.Trans.Maybe         (MaybeT (..))
+import           Control.Monad.Trans.Reader        (ReaderT (..))
+import qualified Control.Monad.Trans.RWS.Lazy      as LRWS
+import qualified Control.Monad.Trans.RWS.Strict    as SRWS
+import qualified Control.Monad.Trans.State.Lazy    as LSt
+import qualified Control.Monad.Trans.State.Strict  as SSt
+import qualified Control.Monad.Trans.Writer.Lazy   as LW
+import qualified Control.Monad.Trans.Writer.Strict as SW
 import           Data.Functor.Identity
+
+import Data.Monoid (Monoid, mempty)
 
 -- -----------------------------------------------------------------------------
 
@@ -26,23 +38,56 @@ class (Applicative m, Monad m) => MonadTower_ m where
   type BaseMonad m :: * -> *
   type BaseMonad m = m
 
+type MonadTower m = ( MonadTower_ m, MonadTower_ (BaseMonad m)
+                    , BaseMonad (BaseMonad m) ~ BaseMonad m
+                    , BaseMonad m ~ BaseMonad (BaseMonad m))
+
+-- -----------------------------------------------------------------------------
+
 instance MonadTower_ []
 instance MonadTower_ Maybe
 instance MonadTower_ IO
 instance MonadTower_ (Either e)
 instance MonadTower_ ((->) r)
+instance (Monad m) => MonadTower_ (WrappedMonad m)
 
 instance MonadTower_ Identity
-
-instance (MonadTower m) => MonadTower_ (LSt.StateT s m) where
-  type BaseMonad (LSt.StateT s m) = BaseMonad m
 
 instance (MonadTower m) => MonadTower_ (ContT r m) where
   type BaseMonad (ContT r m) = BaseMonad m
 
-type MonadTower m = ( MonadTower_ m, MonadTower_ (BaseMonad m)
-                    , BaseMonad (BaseMonad m) ~ BaseMonad m
-                    , BaseMonad m ~ BaseMonad (BaseMonad m))
+instance (MonadTower m) => MonadTower_ (ExceptT e m) where
+  type BaseMonad (ExceptT e m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (IdentityT m) where
+  type BaseMonad (IdentityT m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (ListT m) where
+  type BaseMonad (ListT m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (MaybeT m) where
+  type BaseMonad (MaybeT m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (ReaderT r m) where
+  type BaseMonad (ReaderT r m) = BaseMonad m
+
+instance (Monoid w, MonadTower m) => MonadTower_ (LRWS.RWST r w s m) where
+  type BaseMonad (LRWS.RWST r w s m) = BaseMonad m
+
+instance (Monoid w, MonadTower m) => MonadTower_ (SRWS.RWST r w s m) where
+  type BaseMonad (SRWS.RWST r w s m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (LSt.StateT s m) where
+  type BaseMonad (LSt.StateT s m) = BaseMonad m
+
+instance (MonadTower m) => MonadTower_ (SSt.StateT s m) where
+  type BaseMonad (SSt.StateT s m) = BaseMonad m
+
+instance (Monoid w, MonadTower m) => MonadTower_ (LW.WriterT w m) where
+  type BaseMonad (LW.WriterT w m) = BaseMonad m
+
+instance (Monoid w, MonadTower m) => MonadTower_ (SW.WriterT w m) where
+  type BaseMonad (SW.WriterT w m) = BaseMonad m
 
 -- -----------------------------------------------------------------------------
 
@@ -63,23 +108,79 @@ class (MonadTower m, MonadTower (LowerMonad m)
               -> LowerMonad m (InnerValue m a))
            -> m a
 
+-- -----------------------------------------------------------------------------
+
+instance (MonadTower m) => MonadLevel (ContT r m) where
+  type LowerMonad (ContT r m) = m
+  type InnerValue (ContT r m) a = r
+  type AllConstraintsThrough (ContT r m) = False
+
+  wrap f = ContT $ \ cont -> f (`runContT` cont) (>>= cont)
+
+instance (MonadTower m) => MonadLevel (ExceptT e m) where
+  type LowerMonad (ExceptT e m) = m
+  type InnerValue (ExceptT e m) a = Either e a
+
+  wrap f = ExceptT $ f runExceptT (fmap Right)
+
+instance (MonadTower m) => MonadLevel (IdentityT m) where
+  type LowerMonad (IdentityT m) = m
+
+  wrap f = IdentityT $ f runIdentityT id
+
+instance (MonadTower m) => MonadLevel (ListT m) where
+  type LowerMonad (ListT m) = m
+  type InnerValue (ListT m) a = [a]
+  type AllConstraintsThrough (ListT m) = False
+
+  wrap f = ListT $ f runListT (fmap (:[]))
+
+instance (MonadTower m) => MonadLevel (MaybeT m) where
+  type LowerMonad (MaybeT m) = m
+  type InnerValue (MaybeT m) a = Maybe a
+
+  wrap f = MaybeT $ f runMaybeT (fmap Just)
+
+instance (MonadTower m) => MonadLevel (ReaderT r m) where
+  type LowerMonad (ReaderT r m) = m
+
+  wrap f = ReaderT $ \ r -> f (`runReaderT` r) id
+
+instance (Monoid w, MonadTower m) => MonadLevel (LRWS.RWST r w s m) where
+  type LowerMonad (LRWS.RWST r w s m) = m
+  type InnerValue (LRWS.RWST r w s m) a = (a,s,w)
+
+  wrap f = LRWS.RWST $ \ r s -> f (\m -> LRWS.runRWST m r s) (fmap (,s,mempty))
+
+instance (Monoid w, MonadTower m) => MonadLevel (SRWS.RWST r w s m) where
+  type LowerMonad (SRWS.RWST r w s m) = m
+  type InnerValue (SRWS.RWST r w s m) a = (a,s,w)
+
+  wrap f = SRWS.RWST $ \ r s -> f (\m -> SRWS.runRWST m r s) (fmap (,s,mempty))
+
 instance (MonadTower m) => MonadLevel (LSt.StateT s m) where
-
   type LowerMonad (LSt.StateT s m) = m
-
   type InnerValue (LSt.StateT s m) a = (a,s)
 
   wrap f = LSt.StateT $ \ s -> f (`LSt.runStateT` s) (fmap (,s))
 
-instance (MonadTower m) => MonadLevel (ContT r m) where
+instance (MonadTower m) => MonadLevel (SSt.StateT s m) where
+  type LowerMonad (SSt.StateT s m) = m
+  type InnerValue (SSt.StateT s m) a = (a,s)
 
-  type LowerMonad (ContT r m) = m
+  wrap f = SSt.StateT $ \ s -> f (`SSt.runStateT` s) (fmap (,s))
 
-  type InnerValue (ContT r m) a = r
+instance (Monoid w, MonadTower m) => MonadLevel (LW.WriterT w m) where
+  type LowerMonad (LW.WriterT w m) = m
+  type InnerValue (LW.WriterT w m) a = (a,w)
 
-  type AllConstraintsThrough (ContT r m) = False
+  wrap f = LW.WriterT $ f LW.runWriterT (fmap (,mempty))
 
-  wrap f = ContT $ \ cont -> f (`runContT` cont) (>>= cont)
+instance (Monoid w, MonadTower m) => MonadLevel (SW.WriterT w m) where
+  type LowerMonad (SW.WriterT w m) = m
+  type InnerValue (SW.WriterT w m) a = (a,w)
+
+  wrap f = SW.WriterT $ f SW.runWriterT (fmap (,mempty))
 
 -- -----------------------------------------------------------------------------
 
